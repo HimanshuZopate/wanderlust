8d325d690ddbafc18bfad34281b06ae626b127ed
Himanshu_Zopate <himanshuzopate@gmail.com>
2025-10-05 22:13:53 +0530

    Fixed review.rating validation error and made final updates

--- models/listing.js
+++ models/listing.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Review = require(".review.js");
+const review = require("./review");
 
 const listingSchema = new Schema({
   title: { type: String, required: true },
@@ -8,12 +10,25 @@ const listingSchema = new Schema({
     filename: { type: String, default: "listingimage" },
     url: {
       type: String,
-      default: "https://cdn.pixabay.com/photo/2023/03/06/13/58/logo-7833524_1280.png"
-    }
+      default:
+        "https://cdn.pixabay.com/photo/2023/03/06/13/58/logo-7833524_1280.png",
+    },
   },
   price: Number,
   location: String,
   country: String,
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
+    },
+  ],
+});
+
+listingSchema.post("findOneAndDelete", async (listing) => {
+  if (listing) {
+    await Review.deleteMany({ _id: { $in: listing.reviews } });
+  }
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
