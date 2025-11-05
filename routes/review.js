2f7e5377dcfccbbbb41124815677f650f4ef24ae
Himanshu_Zopate <himanshuzopate@gmail.com>
2025-11-05 14:49:26 +0530

    Added flash messages, express-session, cookies, and connect-flash setup

---
+++ routes/review.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const router = express.Router({ mergeParams: true });
+const wrapAsync = require("../utils/wrapAsync.js");
+const ExpressError = require("../utils/ExpressError.js");
+const { reviewSchema } = require("../schema.js");
+const Review = require("../models/review.js");
+const Listing = require("../models/listing.js"); 
+
+// Middleware for validation
+const validateReview = (req, res, next) => {
+  let { error } = reviewSchema.validate(req.body);
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
+  }
+};
+
+// POST Review Route
+router.post(
+  "/",
+  validateReview,
+  wrapAsync(async (req, res) => {
+    const listing = await Listing.findById(req.params.id);
+    const newReview = new Review(req.body.review);
+
+    listing.reviews.push(newReview._id);
+
+    await newReview.save();
+    await listing.save();
+
+    req.flash("success","New Review Created")
+    res.redirect(`/listings/${listing._id}`);
+  })
+);
+
+// DELETE Review Route
+router.delete(
+  "/:reviewId",
+  wrapAsync(async (req, res) => {
+    const { id, reviewId } = req.params;
+    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    req.flash("success","Review Deleted!")
+    res.redirect(`/listings/${id}`);
+  })
+);
+
+module.exports = router;
